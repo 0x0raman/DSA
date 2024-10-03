e142c8067a622c0a0f44db811931dbb85c07632a
0x0raman <vedraman40@gmail.com>
2024-10-03 22:08:20 +0530

    official acc 03 oct

---
+++ Acc/fup/UniqueCharCount.java
@@ -0,0 +1,31 @@
+package Acc.fup;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * UniqueCharCount
+ */
+public class UniqueCharCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(unique(s));
+        sc.close();
+    }
+
+    private static int unique(String s) {
+        Set<Character> set = new HashSet<Character>();
+        s = s.replaceAll("[{}]", "");
+        String[] e = s.split(",");
+        // int count = 0;
+        for (String e1 : e) {
+            for (char i : e1.trim().toCharArray()) {
+                set.add(i);
+                // count++;
+            }
+        }
+        return set.size();
+    }
+}
