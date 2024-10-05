531c1b66d99b93a30ed53002eb5349ff173ada76
0x0raman <vedraman40@gmail.com>
2024-10-05 20:33:53 +0530

    oct 3 end

--- Acc/fup/UniqueCharCount.java
+++ Acc/fup/UniqueCharCount.java
@@ -17,20 +17,20 @@ public class UniqueCharCount {
 
     private static int unique(String s) {
         Set<Character> set = new HashSet<Character>();
-        // s = s.replaceAll("[{}]", "");
-        // String[] e = s.split(",");
-        // int count = 0;
-        // for (String e1 : e) {
-        //     for (char i : e1.trim().toCharArray()) {
-        //         set.add(i);
-        //         // count++;
-        //     }
-        // }
-        // return set.size();
         for (char i : s.toCharArray()) {
-            set.add(i);
-            // if(s.charAt(i) == ',') return set.size();
+            if(i != ',')
+                set.add(i);
         }
         return set.size();
     }
 }
+// s = s.replaceAll("[{}]", "");
+// String[] e = s.split(",");
+// int count = 0;
+// for (String e1 : e) {
+//     for (char i : e1.trim().toCharArray()) {
+//         set.add(i);
+//         // count++;
+//     }
+// }
+// return set.size();
