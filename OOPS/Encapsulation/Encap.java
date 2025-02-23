ec76a7dcf924971b57538a81c4b85d88c9a450e3
0x0raman <vedraman40@outlook.com>
2025-02-23 19:21:24 +0530

    oop

---
+++ OOPS/Encapsulation/Encap.java
@@ -0,0 +1,11 @@
+package OOPS.Encapsulation;
+
+public class Encap {
+    public static void main(String[] args) {
+        Person obj = new Person();
+        obj.name = "John";
+        System.out.println(obj.name);
+        // obj.setName("JFK");
+        // System.out.println(obj.getName());
+    }
+}
---
+++ OOPS/Encapsulation/Person.java
@@ -0,0 +1,11 @@
+package OOPS.Encapsulation;
+
+public class Person{
+    public String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+}
