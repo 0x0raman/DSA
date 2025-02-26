c4ac8a62b32c144b92fbfa8f4c9034b3ba59d275
0x0raman <vedraman40@outlook.com>
2025-02-26 19:56:35 +0530

    Bucket Sort

--- neetcode/_5TopKElements.java
+++ neetcode/_5TopKElements.java
@@ -69,9 +69,29 @@ public class _5TopKElements {
     //SC: O(n+k)
 
     private int[] heapTopKFrequentOptimised(int[] nums, int k) { //Bucket Sort
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        @SuppressWarnings("unchecked")
+        List<Integer>[] freq = new List[nums.length + 1];
+        for(int i = 0; i < freq.length; i++){
+            freq[i] = new ArrayList<>();
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            freq[entry.getValue()].add(entry.getKey());
+        }
         int[] res = new int[k];
-
+        int index = 0;
+        for(int i = freq.length - 1; i < 0 && index > k; i--){
+            for(int num : freq[i]){
+                res[index++] = num;
+                if(index == k) return res;
+            }
+        }
         return res;
     }
 }
+
+//TC: O(n)
+//SC: O(n)
